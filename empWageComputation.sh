f9f9c6d9108ff32de61f186c6c818230945b63fa
Birajit <birajit95@gmail.com>
2020-10-19 09:12:05 +0530

    Add: get_attendance() function is added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,16 @@
 #! /bin/bash 
 
 echo 'Welcome to Employee Wage Computation Program'
+
+
+get_attendance(){
+
+    employee_attendance=$(( RANDOM%6 ))
+    if [ $employee_attendance -ne 0 ]
+    then
+        echo 1
+    else
+        echo 0
+    fi
+
+}
